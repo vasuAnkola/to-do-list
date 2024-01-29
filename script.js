b5fe78277923b4d30e13494ec7ce37d847398dfc
vasuAnkola <ankolavasu@gmail.com>
2024-01-29 18:21:34 +0530

    local storage

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
-    
+    saveData();
 }
 
 
@@ -23,13 +23,21 @@ function addTask() {
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        
+        saveData();
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        
+        saveData();
     }
 
 }, false);
 
 
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+    
+}
 
+function showtask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
